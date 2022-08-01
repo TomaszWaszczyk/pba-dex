60f97bb47300caea7d69d14998e92fc41d6b7e72
Tomasz Waszczyk <tomasz@waszczyk.com>
2022-08-01 14:32:13 +0200

    added(): basic enums

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -35,13 +35,6 @@ pub mod pallet {
 	pub struct Pallet<T>(_);
 
 
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-
-	pub type Something<T> = StorageValue<_, u32>;
-
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/v3/runtime/events-and-errors
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
@@ -58,42 +51,36 @@ pub mod pallet {
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
+		pub fn swap(origin: OriginFor<T>, something: u32) -> DispatchResult {
 			// Check that the extrinsic was signed and get the signer.
 			// This function will return an error if the extrinsic is not signed.
 			// https://docs.substrate.io/v3/runtime/origins
 			let who = ensure_signed(origin)?;
 
-			// Update storage.
-			<Something<T>>::put(something);
 
 			// Return a successful DispatchResultWithPostInfo
 			Ok(())
 		}
+	}
 
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+	#[derive(Encode, Decode, Clone)]
+	#[cfg_attr(feature = "std", derive(Debug))]
+	pub enum OrderStatus {
+    	Created,
+    	Filled,
+    	Canceled,
+	}
 
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => return Err(Error::<T>::NoneValue.into()),
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
-		}
+	#[derive(Encode, Decode)]
+	pub enum OrderType {
+    	Buy,
+    	Sell,
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
 		NoneValue,
-		StorageOverflow,
+		BalanceNotEnough,
+		NoLiquidity,
 	}
 }
